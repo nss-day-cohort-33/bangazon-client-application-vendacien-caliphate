8a454017e6ee2eca309fc8fa4c42d1814ca2cf0b
Michael Drew Palazola <mpalazola@hotmail.com>
2019-10-08 11:30:53 -0500

    login working and detail product page

--- bangazon-client/src/components/ApplicationViews.js
+++ bangazon-client/src/components/ApplicationViews.js
@@ -4,6 +4,7 @@ import { withRouter } from "react-router-dom"
 import Register from "./auth/Register"
 import Login from "./auth/Login"
 import HomePage from "./home/HomePage"
+import ProductDetail from "./product/ProductDetail"
 
 
 const ApplicationViews = () => {
@@ -28,6 +29,11 @@ const ApplicationViews = () => {
                 }}
             />
 
+            <Route exact path="/products/:productId(\d+)" render={(props) => {
+                return <ProductDetail {...props} />
+            }}
+
+            />
 
 
         </React.Fragment>
