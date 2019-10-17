830085096c0649519ab50ab8a3394ef56c9cc489
Michael Drew Palazola <mpalazola@hotmail.com>
2019-10-17 15:38:09 -0500

    order is completed

--- bangazon-client/src/components/Order/CurrentOrder.js
+++ bangazon-client/src/components/Order/CurrentOrder.js
@@ -1,42 +1,104 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react";
 
 const MyCart = props => {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
+  const [paymenttypes, setPaymentTypes] = useState([]);
+  const payment = useRef();
 
   const getOpenOrder = () => {
     fetch(`http://localhost:8000/orders/cart`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        "Accept": "application/json",
-        "Authorization": `Token ${localStorage.getItem("bangazon_token")}`
+        Accept: "application/json",
+        Authorization: `Token ${localStorage.getItem("bangazon_token")}`
       }
     })
-    .then(response => {
-      console.log('response', response );
-      return response.json()
-    })
-    .then(setProducts)
-  }
+      .then(response => {
+        console.log("response", response);
+        return response.json();
+      })
+      .then(setProducts);
+  };
 
-  useEffect(getOpenOrder, [])
-    console.log('Open order', products)
+  const getPaymentTypes = () => {
+    fetch(
+      `http://localhost:8000/paymenttypes?customer_id=${localStorage.getItem(
+        "customer_id"
+      )}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Token ${localStorage.getItem("bangazon_token")}`
+        }
+      }
+    )
+      .then(response => {
+        console.log("response", response);
+        return response.json();
+      })
+      .then(setPaymentTypes);
+  };
+
+  const completeOrder = () => {
+    fetch(`http://localhost:8000/orders/cart`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Token ${localStorage.getItem("bangazon_token")}`
+      },
+      body: JSON.stringify({
+        "payment_id": payment.current.value
+      })
+    })
+    .then(() => {
+      props.history.push("/")
+    })
+  };
+
+  useEffect(() => {
+    getOpenOrder();
+    getPaymentTypes();
+  }, []);
+
+  console.log("ORDER", products);
   return (
     <>
       <main className="order-items">
         <h2>My Cart</h2>
         <ul>
-          {
-            products.map(item => {
-                    return (<li key={item.id}>{item.name}: {item.price}</li>)
-                })
-
-          }
+          {products.map(item => {
+            return (
+              <li key={item.id}>
+                {item.name}: {item.price}
+              </li>
+            );
+          })}
         </ul>
-        <button>Add Payment to complete order</button>
+        <label htmlFor="paymenttypes"> Select a Payment: </label>
+        <select ref={payment}>
+          <option value="" >
+            Select a payment type...
+          </option>
+          {paymenttypes.map(payment => (
+            <option key={payment.id} value={payment.id}>
+              {payment.merchant_name}
+            </option>
+          ))}
+        </select>
+        <button
+          className="fakeLink addToOrder__link"
+          onClick={() => completeOrder()}
+        >
+          {" "}
+          Complete Order{" "}
+        </button>
       </main>
     </>
-  )
-}
+  );
+};
 
-export default MyCart
+export default MyCart;
